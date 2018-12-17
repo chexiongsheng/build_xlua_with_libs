170899bd5bf48defd33790e0a435423e3afba8b1
jayatubi <yzk0370@gmail.com>
2018-12-17 20:38:21 +0800

    修复lua-protobuf luajit编译错误

---
+++ build/make_win32_luajit_vs2017.bat
@@ -0,0 +1,22 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+
+echo Swtich to x86 build env
+cd %~dp0\luajit-2.1.0b3\src
+call msvcbuild_mt.bat static
+cd ..\..
+
+goto :buildxlua
+
+:missing
+echo Can't find Visual Studio 2015.
+pause
+goto :eof
+
+:buildxlua
+mkdir build_lj32 & pushd build_lj32
+cmake -DUSING_LUAJIT=ON -G "Visual Studio 15 2017" ..
+popd
+cmake --build build_lj32 --config Release
+md plugin_luajit\Plugins\x86
+copy /Y build_lj32\Release\xlua.dll plugin_luajit\Plugins\x86\xlua.dll
+pause
